f5e26f1abf254c57ce65cb0b060d9742ac07f98d
mickdowling16 <>
2023-10-29 18:24:24 +0000

    add events to homepage to show 3 nearest events

--- home/views.py
+++ home/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
+from events.models import Event
+from datetime import datetime
 
 
 def index(request):
-    """ A view to return the index page """
-
-    return render(request, 'home/index.html')
+    current_date = datetime.now()
+    upcoming_events = Event.objects.filter(
+        date__gte=current_date).order_by('date')[:3]
+    return render(request, 'home/index.html', {'upcoming_events': upcoming_events})
