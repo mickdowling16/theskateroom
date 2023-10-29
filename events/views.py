29cb59a396cb69f564a5f48683e1b96159499714
mickdowling16 <>
2023-10-29 17:15:13 +0000

    create event app. add in basic model information and view. Create first event from the admin panel

---
+++ events/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Event
+
+
+def event_list(request):
+    events = Event.objects.all()
+    return render(request, 'events/event_list.html', {'events': events})
+
+
+def event_detail(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+    return render(request, 'events/event_detail.html', {'event': event})
