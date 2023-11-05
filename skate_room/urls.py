2b4945d017a7260a4d989f889d152243a4e1f003
mickdowling16 <>
2023-11-05 14:40:11 +0000

    add checkout form and update styling on homepage

--- skate_room/urls.py
+++ skate_room/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('events/', include('events.urls')),
     path('', include('staticpages.urls')),
     path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
