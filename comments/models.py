233eed444a4c6dea3e7d7aedb502fc34235c5651
mickdowling16 <>
2023-11-06 15:28:29 +0000

    add javascript to handle adding comments and likes to events

--- comments/models.py
+++ comments/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from profiles.models import UserProfile
+from django.contrib.auth.models import User
 from events.models import Event
-from django.utils import timezone
 
 
 class Comment(models.Model):
+    event = models.ForeignKey(
+        Event, on_delete=models.CASCADE, related_name='comments')
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
     text = models.TextField()
-    created_at = models.DateTimeField(default=timezone.now)
-    parent_comment = models.ForeignKey(
-        'self', null=True, blank=True, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='liked_comments')
+
+    def __str__(self):
+        return self.text
--- comments/urls.py
+++ comments/urls.py
@@ -1,10 +1,9 @@
-# comments/urls.py
 from django.urls import path
 from . import views
 
 app_name = 'comments'
-
 urlpatterns = [
     path('add_comment/<int:event_id>/', views.add_comment, name='add_comment'),
-    path('like_comment/', views.like_comment, name='like_comment'),
+    path('like_comment/<int:comment_id>/',
+         views.like_comment, name='like_comment'),
 ]
