150c644411ed8edc2a0f0b58b3fa507ad3e5e78f
mickdowling16 <>
2023-11-08 20:45:57 +0000

    add view, template and url to edit events on the website

--- events/urls.py
+++ events/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.event_list, name='event_list'),
     path('<int:event_id>/', views.event_detail, name='event_detail'),
     path('register/', views.register, name='register'),
-    path('events/add/', views.add_event, name='add_event')
+    path('events/add/', views.add_event, name='add_event'),
+    path('edit/<int:event_id>/', views.edit_event, name='edit_event'),
 ]
