ba11a412043047dd6bc8e9e67466b6e41e2acc12
mickdowling16 <michaeldowling16@gmail.com>
2023-11-10 15:19:34 +0000

    add secret key to settings.py file

--- skate_room/settings.py
+++ skate_room/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)(7_8=44jtc5&23&-#66zlhn)%qj2&7vmx$vm6(xp%k_4p%twb'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['skate-room-fa0d77babcb9.herokuapp.com', '8000-mickdowling-theskateroo-3q138vatimo.ws-eu106.gitpod.io']
 
