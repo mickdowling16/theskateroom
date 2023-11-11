76474fe28fad1f5d093d1460851ff8d0c839359a
mickdowling16 <michaeldowling16@gmail.com>
2023-11-11 14:59:15 +0000

    added to add to bag view to check stock levels before adding to bag, and remove stock if item is added to bag

--- bag/views.py
+++ bag/views.py
@@ -19,32 +19,69 @@ def add_to_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
+
     if 'product_size' in request.POST:
         size = request.POST['product_size']
+
     bag = request.session.get('bag', {})
 
     if size:
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
-                bag[item_id]['items_by_size'][size] += quantity
-                messages.success(
-                    request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
+                # Check if adding the requested quantity exceeds the available stock
+                if product.stock >= quantity:
+                    bag[item_id]['items_by_size'][size] += quantity
+                    product.stock -= quantity
+                    product.save()
+                    messages.success(
+                        request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
+                else:
+                    messages.error(
+                        request, f'Not enough stock available for {product.name}.')
             else:
-                bag[item_id]['items_by_size'][size] = quantity
+                # Check if adding the requested quantity exceeds the available stock
+                if product.stock >= quantity:
+                    bag[item_id]['items_by_size'][size] = quantity
+                    product.stock -= quantity
+                    product.save()
+                    messages.success(
+                        request, f'Added size {size.upper()} {product.name} to your bag')
+                else:
+                    messages.error(
+                        request, f'Not enough stock available for {product.name}.')
+        else:
+            # Check if adding the requested quantity exceeds the available stock
+            if product.stock >= quantity:
+                bag[item_id] = {'items_by_size': {size: quantity}}
+                product.stock -= quantity
+                product.save()
                 messages.success(
                     request, f'Added size {size.upper()} {product.name} to your bag')
-        else:
-            bag[item_id] = {'items_by_size': {size: quantity}}
-            messages.success(
-                request, f'Added size {size.upper()} {product.name} to your bag')
+            else:
+                messages.error(
+                    request, f'Not enough stock available for {product.name}.')
     else:
         if item_id in list(bag.keys()):
-            bag[item_id] += quantity
-            messages.success(
-                request, f'Updated {product.name} quantity to {bag[item_id]}')
+            # Check if adding the requested quantity exceeds the available stock
+            if product.stock >= quantity:
+                bag[item_id] += quantity
+                product.stock -= quantity
+                product.save()
+                messages.success(
+                    request, f'Updated {product.name} quantity to {bag[item_id]}')
+            else:
+                messages.error(
+                    request, f'Not enough stock available for {product.name}.')
         else:
-            bag[item_id] = quantity
-            messages.success(request, f'Added {product.name} to your bag')
+            # Check if adding the requested quantity exceeds the available stock
+            if product.stock >= quantity:
+                bag[item_id] = quantity
+                product.stock -= quantity
+                product.save()
+                messages.success(request, f'Added {product.name} to your bag')
+            else:
+                messages.error(
+                    request, f'Not enough stock available for {product.name}.')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
